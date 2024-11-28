f103028e071c998134da6cd3f806b620b574287f
RSagdeev <RSagdeev@kmpo.ru>
2024-11-28 13:55:37 +0300

    Задание 2

--- Task2.sql
+++ Task2.sql
@@ -1,3 +1,3 @@
 ﻿SELECT Product.Name, Category.Name FROM Product 
 	LEFT JOIN ProductCategory on Product.Id=ProductCategory.ProductId
-	JOIN Category on ProductCategory.CategoryId=Category.Id
+	LEFT JOIN Category on ProductCategory.CategoryId=Category.Id
